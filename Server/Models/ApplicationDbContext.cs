1926ac4d58734f3ef3b6aa4312aac9945383b0e1
velvet_orange <ig348838@gmail.com>
2024-08-30 18:10:54 +0300

    Introduce DbContext

---
+++ Server/Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using API.Models.Entitites;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Models;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) {}
+}
