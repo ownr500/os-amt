44775db07b349788248243ace825d3a5b4e003dc
velvet_orange <ig348838@gmail.com>
2024-08-30 17:49:17 +0300

    Introduce constants to validate requests

---
+++ Server/Constants/ValidationConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace API.Constants;
+
+public class ValidationConstants
+{
+    public const int MinAge = 20;
+    public const int MaxLengthName = 20;
+    public const int MinLengthName = 3;
+    public const int MaxLoginLength = 12;
+    public const int MinLoginLength = 5;
+    public const int MaxPasswordLength = 25;
+    public const int MinPasswordLength = 5;
+}
