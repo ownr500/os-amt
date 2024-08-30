7140c51a24f8341a9a0d9773f29ac03ba1d5e41a
velvet_orange <ig348838@gmail.com>
2024-08-30 23:09:01 +0300

    Code improvement

--- Server/Services/UserService.cs
+++ Server/Services/UserService.cs
@@ -34,18 +34,15 @@ public class UserService : IUserService
         return new RegisterResponse(user.Id);
     }
 
-    private string GeneratePasswordHash(string password)
+    private static string GeneratePasswordHash(string password)
     {
-        using (SHA256 sha256 = SHA256.Create())
+        using var sha256 = SHA256.Create();
+        var bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+        StringBuilder builder = new StringBuilder();
+        foreach (var b in bytes)
         {
-            byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
-            StringBuilder builder = new StringBuilder();
-            foreach (var b in bytes)
-            {
-                builder.Append(b.ToString("x2"));
-            }
-
-            return builder.ToString();
+            builder.Append(b.ToString("x2"));
         }
+        return builder.ToString();
     }
 }
