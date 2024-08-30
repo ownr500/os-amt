61c02fd8db3d9ba151bc8edca921b5a92c01c3eb
velvet_orange <ig348838@gmail.com>
2024-08-30 15:50:26 +0300

    Introduce IUserService interface with RegisterAsync description

---
+++ Server/Services/Interfaces/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using API.Controllers.DTO;
+using API.Models.Request;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Services.Interfaces;
+
+public interface IUserService
+{
+    Task<ActionResult<RegisterResponseDto>> RegisterAsync(RegisterRequest request);
+}
