09af7bfc2de1212dba9c8160a328fc99f9fcb425
velvet_orange <ig348838@gmail.com>
2024-08-30 15:48:50 +0300

    Add extension method RegisterRequestDto

---
+++ Server/Extensions/Mappers.cs
@@ -0,0 +1,18 @@
+﻿using API.Controllers.DTO;
+using API.Models.Request;
+
+namespace API.Extensions;
+
+public static class Mappers
+{
+    public static RegisterRequest ToRequest(this RegisterRequestDto dto)
+    {
+        return new RegisterRequest(
+            dto.FirstName,
+            dto.LastName,
+            dto.Age,
+            dto.Login,
+            dto.Password
+        );
+    }
+}
