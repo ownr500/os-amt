a2864f625814612672e52a1a2612f7cde2fcf594
velvet_orange <ig348838@gmail.com>
2024-08-31 16:09:17 +0300

    Add change request and response DTOs

---
+++ Server/Controllers/DTO/ChangeRequestDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using API.Constants;
+
+namespace API.Controllers.DTO;
+
+public record ChangeRequestDto(
+    [StringLength(ValidationConstants.MaxLengthName, MinimumLength = ValidationConstants.MinLengthName)]
+    [property: JsonPropertyName("firstName")]
+    string FirstName,
+    [property: JsonPropertyName("age")]
+    [Range(ValidationConstants.MinAge, int.MaxValue)]
+    int Age,
+    [property: JsonPropertyName("password")]
+    [StringLength(ValidationConstants.MaxPasswordLength, MinimumLength = ValidationConstants.MinPasswordLength)]
+    string Password
+);
---
+++ Server/Controllers/DTO/ChangeResponseDto.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace API.Controllers.DTO;
+
+public record ChangeResponseDto(
+    [property: JsonPropertyName("success")]
+    bool Success,
+    [property: JsonPropertyName("errorMessage")]
+    string? ErrorMessage
+);
