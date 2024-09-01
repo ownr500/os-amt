8b38ca330f8f3fa2d2407fa06d4d846b22ab8269
velvet_orange <ig348838@gmail.com>
2024-09-01 18:33:24 +0300

    Db Table constraints, add LoginNormalized, create index

---
+++ Server/Models/Entitites/UserEntity.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using API.Constants;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Models.Entitites;
+
+[Index(nameof(LoginNormalized), IsUnique = true)]
+public class UserEntity
+{
+    [Key] public Guid Id { get; set; }
+
+    [MaxLength(ValidationConstants.MaxLengthName)]
+    public string FirstName { get; set; }
+
+    [MaxLength(ValidationConstants.MaxLengthName)]
+    public string LastName { get; set; }
+
+    public int Age { get; set; }
+
+    [MaxLength(ValidationConstants.MaxLoginLength)]
+    public string Login { get; set; }
+
+    [MaxLength(ValidationConstants.MaxLoginLength)]
+    public string LoginNormalized { get; set; }
+
+    [MaxLength(ValidationConstants.MaxPasswordHashLength)]
+    public string PasswordHash { get; set; }
+}
