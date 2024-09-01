f97e4e69c267e0ddce1e0666b017ecd569f94605
velvet_orange <ig348838@gmail.com>
2024-09-01 18:37:51 +0300

    Introduce Password and User controllers

---
+++ Server/Controllers/PasswordController.cs
@@ -0,0 +1,25 @@
+﻿using API.Controllers.DTO;
+using API.Extensions;
+using API.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PasswordController
+{
+    private readonly IUserService _userService;
+
+    public PasswordController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Change([FromBody] PasswordChangeDto passwordChangeDto)
+    {
+        var result = await _userService.PasswordChangeAsync(passwordChangeDto.ToModel());
+        return result ? new OkResult() : new BadRequestResult();
+    }
+}
---
+++ Server/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using API.Controllers.DTO;
+using API.Extensions;
+using API.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+    
+    [HttpDelete]
+    public async Task<ActionResult<DeleteResponseDto>> Delete([FromQuery] string login)
+    {
+        var response = await _userService.DeleteAsync(login);
+        return response.ToDto();
+    }
+
+    [HttpPatch]
+    public async Task<ActionResult<ChangeResponseDto>> Change([FromBody] ChangeRequestDto requestDto)
+    {
+        var response = await _userService.ChangeAsync(requestDto.ToRequest());
+        return response.ToDto();
+    }
+}
