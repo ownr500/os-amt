ceb2223409bab6603ded39d1120059e0e17ebd22
velvet_orange <ig348838@gmail.com>
2024-09-02 21:40:41 +0300

    Password change endpoint improvement with jwt token Authorization

--- Server/Controllers/PasswordController.cs
+++ Server/Controllers/PasswordController.cs
@@ -1,4 +1,5 @@
-﻿using API.Controllers.DTO;
+﻿using System.Security.Claims;
+using API.Controllers.DTO;
 using API.Extensions;
 using API.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
@@ -12,16 +13,41 @@ namespace API.Controllers;
 public class PasswordController
 {
     private readonly IUserService _userService;
+    private readonly HttpContextAccessor _contextAccessor;
+    private readonly ITokenService _tokenService;
 
-    public PasswordController(IUserService userService)
+    public PasswordController(IUserService userService, HttpContextAccessor contextAccessor, ITokenService tokenService)
     {
         _userService = userService;
+        _contextAccessor = contextAccessor;
+        _tokenService = tokenService;
     }
 
     [HttpPost]
     public async Task<IActionResult> Change([FromBody] PasswordChangeDto passwordChangeDto)
     {
-        var result = await _userService.PasswordChangeAsync(passwordChangeDto.ToModel());
-        return result ? new OkResult() : new BadRequestResult();
+        var authHeader = _contextAccessor.HttpContext.Request.Headers["Authorization"].ToString();
+        if (string.IsNullOrEmpty(authHeader) || !authHeader.StartsWith("Bearer"))
+        {
+            return new UnauthorizedResult();
+        }
+
+        var token = authHeader.Substring("Bearer ".Length).Trim();
+
+        try
+        {
+            var claimsPrincipal = await _tokenService.ValidateToken(token);
+            var userId = claimsPrincipal.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (userId != null)
+            {
+                var result = await _userService.PasswordChangeAsync(passwordChangeDto.ToModel(new Guid(userId)));
+                return result ? new OkResult() : new BadRequestResult();
+            }
+        }
+        catch (Exception e)
+        {
+            throw new BadHttpRequestException("Something went wrong");
+        }
+        return new BadRequestResult();
     }
 }
