d9b233c62173ff39bed83d74be56005d2f98600d
velvet_orange <ig348838@gmail.com>
2024-09-02 21:00:57 +0300

    Add TokenEntity

--- Server/Models/ApplicationDbContext.cs
+++ Server/Models/ApplicationDbContext.cs
@@ -6,7 +6,8 @@ namespace API.Models;
 public class ApplicationDbContext : DbContext
 {
     public DbSet<UserEntity> Users { get; set; }
-    
+    public DbSet<TokenEntity> Tokens { get; set; }
+
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options) {}
 }
---
+++ Server/Models/Entitites/TokenEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using API.Constants;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Models.Entitites;
+
+[Index(nameof(JwtToken), IsUnique = true)]
+public class TokenEntity
+{
+    [Key] public Guid Id { get; set; }
+
+    [MaxLength(ValidationConstants.MaxTokenLength)]
+    public string JwtToken { get; set; }
+}
