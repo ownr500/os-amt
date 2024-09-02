d335f07fc3802ea300a1b5e79c2ae163bc9d3d66
velvet_orange <ig348838@gmail.com>
2024-09-02 19:58:33 +0300

    start to implement Jwt logic

--- Server/Controllers/AuthController.cs
+++ Server/Controllers/AuthController.cs
@@ -2,6 +2,7 @@
 using API.Extensions;
 using API.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace API.Controllers;
 
@@ -22,5 +23,29 @@ public class AuthController : ControllerBase
     {
         var response = await _userService.RegisterAsync(requestDto.ToRequest());
         return response.ToDto();
+    }
+
+    [HttpPost("signin")]
+    public async Task<ActionResult<SinginResponseDto>> Signin([FromBody] SigninRequestDto signinRequestDto)
+    {
+        var user = await _userService.Singin(signinRequestDto.ToModel());
+
+
+        // var authHeader = HttpContext.Request.Headers["Authorization"].ToString();
+        // if (string.IsNullOrEmpty(authHeader) || !authHeader.StartsWith("Bearer"))
+        // {
+        //     return Unauthorized();
+        // }
+        //
+        // var token = authHeader.Substring("Bearer".Length).Trim();
+        //
+        // try
+        // {
+        //     var claimsPrincipal = 
+        // }
+        // catch (SecurityTokenException  e)
+        // {
+        //     return Unauthorized("Invalid token");
+        // }
     }
 }
