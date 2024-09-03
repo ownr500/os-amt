c91eb59f748658a98112551d6f1d574f4f4b6d35
velvet_orange <ig348838@gmail.com>
2024-09-03 13:20:53 +0300

    Adjust naming in TokenEntity

--- Server/Models/Entitites/TokenEntity.cs
+++ Server/Models/Entitites/TokenEntity.cs
@@ -4,11 +4,11 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Models.Entitites;
 
-[Index(nameof(JwtToken), IsUnique = true)]
+[Index(nameof(AuthToken), IsUnique = true)]
 public class TokenEntity
 {
     [Key] public Guid Id { get; set; }
 
     [MaxLength(ValidationConstants.MaxTokenLength)]
-    public string JwtToken { get; set; }
+    public string AuthToken { get; set; }
 }
