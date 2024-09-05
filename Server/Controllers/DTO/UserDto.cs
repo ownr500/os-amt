f67b8cb6f26679f0e8fca880dbc8fd9148e52da5
velvet_orange <ig348838@gmail.com>
2024-09-05 16:18:02 +0300

    Add UserDto

---
+++ Server/Controllers/DTO/UserDto.cs
@@ -0,0 +1,11 @@
+﻿using API.Models.enums;
+
+namespace API.Controllers.DTO;
+
+public record UserDto(
+    Guid UserId,
+    string FirstName,
+    string LastName,
+    string Login,
+    List<RoleType> Role
+);
