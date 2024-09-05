30a8526ae1225f879c38f30002154f46ce3d297e
velvet_orange <ig348838@gmail.com>
2024-09-05 16:19:46 +0300

    Add Admin controller with GetUsers endpoint

---
+++ Server/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using API.Controllers.DTO;
+using API.Extensions;
+using API.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Authorize]
+[Route("api/[controller]")]
+public class AdminController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public AdminController(IUserService userService)
+    {
+        _userService = userService;
+    }
+    [HttpGet("getUsers")]
+    public async Task<ActionResult<UserListResponseDto>> GetUsers(CancellationToken ct)
+    {
+        var result = await _userService.GetUsers(ct);
+        return new UserListResponseDto(
+            result.ToDto()
+        );
+    }
+}
