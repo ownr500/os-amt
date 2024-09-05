655b07639b1e4124a40a94d95299a44670aa4282
velvet_orange <ig348838@gmail.com>
2024-09-05 18:05:51 +0300

    fix

--- Server/Services/TokenService.cs
+++ Server/Services/TokenService.cs
@@ -37,7 +37,7 @@ public class TokenService : ITokenService
         var claims = new List<Claim>
         {
             new(ClaimTypes.NameIdentifier, userId.ToString()),
-            new(ClaimTypes.Role, RoleName.Admin.ToString())
+            new(ClaimTypes.Role, role)
         };
 
         var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(SecretKey));
