8a3e16b8400ae9a94a0c399f330108f3efe0ccbf
velvet_orange <ig348838@gmail.com>
2024-09-05 23:06:32 +0300

    Update MessageConstants

--- Server/Constants/MessageConstants.cs
+++ Server/Constants/MessageConstants.cs
@@ -9,6 +9,7 @@ public class MessageConstants
     public const string RegistrationFailed = "Registration failed";
     public const string InvalidCredentials = "Invalid credentials";
     public const string OldPasswordNotMatch = "Old password doesn't match";
+    public const string WrongPassword = "Wrong password";
     public const string UserUpdateFailed = "User update failed";
     public const string InvalidRefreshToken = "Invalid refresh token";
     public const string UserHasNoRoles = "User has no roles";
