ef061bfc98554a92897dbd1ffce0378efc56910a
velvet_orange <ig348838@gmail.com>
2024-09-07 23:06:16 +0300

    Introduce UserAdmin controller with AddRole endpoint

---
+++ Server/Controllers/UserAdmin.cs
@@ -0,0 +1,26 @@
+﻿using API.Models.enums;
+using API.Services.Interfaces;
+using FluentResults;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Authorize(Roles = nameof(RoleName.Admin))]
+[Route("api/[controller]")]
+public class UserAdmin : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public UserAdmin(IUserService userService)
+    {
+        _userService = userService;
+    }
+    
+    [HttpPost("{id}/add-role")]
+    public async Task<IActionResult> AddRole([FromRoute] string id, [FromBody] string role)
+    {
+        Result result = await _userService.AddRoleAsync(id, role);
+    }
+}
