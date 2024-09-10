e4ca27b64e1284f553a5ea01f113c214eeb439cd
velvet_orange <ig348838@gmail.com>
2024-09-11 00:06:00 +0300

    Entity change

--- Server/Models/Entities/RevokedTokenEntity.cs
+++ Server/Models/Entities/RevokedTokenEntity.cs
@@ -4,17 +4,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Models.Entities;
 
-[Index(nameof(AccessToken), IsUnique = true)]
-[Index(nameof(RefreshToken), IsUnique = true)]
+[Index(nameof(Token), IsUnique = true)]
 public class RevokedTokenEntity
 {
     [Key] public Guid Id { get; set; }
     
     [MaxLength(ValidationConstants.MaxTokenLength)]
-    public string AccessToken { get; set; } = string.Empty;
+    public string Token { get; set; } = string.Empty;
 
-    [MaxLength(ValidationConstants.MaxTokenLength)]
-    public string RefreshToken { get; set; } = string.Empty;
-    
-    public DateTimeOffset RefreshTokenExpireAt { get; set; }
+    public DateTimeOffset TokenExpireAt { get; set; }
 }
