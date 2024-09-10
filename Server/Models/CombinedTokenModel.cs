53b5736b9bbaeb5922263a86a247d9218cec2523
velvet_orange <ig348838@gmail.com>
2024-09-11 00:05:29 +0300

    Combined model for revoked tokens

---
+++ Server/Models/CombinedTokenModel.cs
@@ -0,0 +1,9 @@
+﻿using API.Models.Entities;
+
+namespace API.Models;
+
+public class CombinedTokenModel
+{
+    public RevokedTokenEntity Entity1 { get; set; }
+    public RevokedTokenEntity Entity2 { get; set; }
+}
