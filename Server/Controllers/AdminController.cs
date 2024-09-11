bdd9a0c8ebbb3c197ecee44d1d7fbae4c4658cfd
velvet_orange <ig348838@gmail.com>
2024-09-11 10:40:40 +0300

    Revoke token endpoint for admin

--- Server/Controllers/AdminController.cs
+++ Server/Controllers/AdminController.cs
@@ -13,10 +13,12 @@ namespace API.Controllers;
 public class AdminController : ControllerBase
 {
     private readonly IUserService _userService;
+    private readonly ITokenService _tokenService;
 
-    public AdminController(IUserService userService)
+    public AdminController(IUserService userService, ITokenService tokenService)
     {
         _userService = userService;
+        _tokenService = tokenService;
     }
     [HttpGet("getUsers")]
     public async Task<ActionResult<UserListResponseDto>> GetUsers(CancellationToken ct)
@@ -31,6 +33,14 @@ public class AdminController : ControllerBase
     public async Task<IActionResult> MakeUserAdmin([FromQuery] Guid userId, CancellationToken ct)
     {
         var result = await _userService.MakeUserAdmin(userId, ct);
+        if (result.IsSuccess) return Ok();
+        return new ConflictObjectResult(new BusinessErrorDto(result.GetErrors()));
+    }
+
+    [HttpPost("revoke/{userId:guid}")]
+    public async Task<IActionResult> Revoke([FromRoute] Guid userId, CancellationToken ct)
+    {
+        var result = await _tokenService.RevokeTokens(userId, ct);
         if (result.IsSuccess) return Ok();
         return new ConflictObjectResult(new BusinessErrorDto(result.GetErrors()));
     }
