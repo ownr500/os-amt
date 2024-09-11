9aabe35e8732177f71e034587c925ff94e325f9d
velvet_orange <ig348838@gmail.com>
2024-09-11 23:28:38 +0300

    Add init migration

---
+++ Server/Migrations/20240911202732_init.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RevokedTokens",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Token = table.Column<string>(type: "nvarchar(2048)", maxLength: 2048, nullable: false),
+                    TokenExpireAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RevokedTokens", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    RoleName = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Login = table.Column<string>(type: "nvarchar(12)", maxLength: 12, nullable: false),
+                    LoginNormalized = table.Column<string>(type: "nvarchar(12)", maxLength: 12, nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tokens",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AccessToken = table.Column<string>(type: "nvarchar(2048)", maxLength: 2048, nullable: false),
+                    RefreshToken = table.Column<string>(type: "nvarchar(2048)", maxLength: 2048, nullable: false),
+                    RefreshTokenActive = table.Column<bool>(type: "bit", nullable: false),
+                    AccessTokenExpireAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    RefreshTokenExpireAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tokens", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tokens_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserRoles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    RoleId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserRoles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserRoles_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserRoles_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "RoleName" },
+                values: new object[,]
+                {
+                    { new Guid("561622cb-ca02-4c14-9c44-21bc4ba4d2ac"), 2 },
+                    { new Guid("c9a36382-bb77-4ee7-8539-681026b43916"), 1 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Age", "FirstName", "LastName", "Login", "LoginNormalized", "PasswordHash" },
+                values: new object[] { new Guid("561bbfaa-c44a-45f9-97c4-7182ba38b85f"), 30, "Admin", "Admin", "admin", "admin", "5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5" });
+
+            migrationBuilder.InsertData(
+                table: "UserRoles",
+                columns: new[] { "Id", "RoleId", "UserId" },
+                values: new object[] { new Guid("db0a2bde-ae9b-44c8-be2f-17e353a17d1a"), new Guid("c9a36382-bb77-4ee7-8539-681026b43916"), new Guid("561bbfaa-c44a-45f9-97c4-7182ba38b85f") });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RevokedTokens_Token",
+                table: "RevokedTokens",
+                column: "Token",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tokens_AccessToken",
+                table: "Tokens",
+                column: "AccessToken",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tokens_RefreshToken",
+                table: "Tokens",
+                column: "RefreshToken",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tokens_UserId",
+                table: "Tokens",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserRoles_RoleId",
+                table: "UserRoles",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserRoles_UserId",
+                table: "UserRoles",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_LoginNormalized",
+                table: "Users",
+                column: "LoginNormalized",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RevokedTokens");
+
+            migrationBuilder.DropTable(
+                name: "Tokens");
+
+            migrationBuilder.DropTable(
+                name: "UserRoles");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- Server/Models/ApplicationDbContext.cs
+++ Server/Models/ApplicationDbContext.cs
@@ -14,7 +14,9 @@ public class ApplicationDbContext : DbContext
     public DbSet<RevokedTokenEntity> RevokedTokens { get; set; } = default!;
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options) {}
+        : base(options)
+    {
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -35,6 +37,28 @@ public class ApplicationDbContext : DbContext
                 RoleName = RoleNames.User
             });
 
+        modelBuilder.Entity<UserEntity>().HasData(
+            new UserEntity
+            {
+                Id = Guid.Parse("561BBFAA-C44A-45F9-97C4-7182BA38B85F"),
+                Age = 30,
+                Login = "admin",
+                FirstName = "Admin",
+                LastName = "Admin",
+                LoginNormalized = "admin",
+                PasswordHash = "5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5"
+            }
+        );
+
+        modelBuilder.Entity<UserRoleEntity>().HasData(
+            new UserRoleEntity
+            {
+                Id = Guid.NewGuid(),
+                UserId = Guid.Parse("561BBFAA-C44A-45F9-97C4-7182BA38B85F"),
+                RoleId = RoleConstants.AdminRoleId
+            }
+        );
+
         modelBuilder.Entity<UserEntity>()
             .HasMany(x => x.UserRoles)
             .WithOne(x => x.User)
@@ -44,7 +68,7 @@ public class ApplicationDbContext : DbContext
             .HasMany(x => x.UserRoles)
             .WithOne(x => x.Role)
             .HasForeignKey(x => x.RoleId);
-        
+
         base.OnModelCreating(modelBuilder);
     }
 }
