dfccb4189fa8afd83935eab10f03d4cc6e919964
velvet_orange <ig348838@gmail.com>
2024-09-12 16:23:59 +0300

    Introduce IEmailService

---
+++ Server/src/Api/Services/Interfaces/IEmailService.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Services.Interfaces;
+
+public interface IEmailService
+{
+    Task<IActionResult> SendRecoveryLink(string email, CancellationToken ct);
+}
