5de03983dc817d5563eceb13bb86e9f9dffe561a
velvet_orange <ig348838@gmail.com>
2024-09-17 10:18:19 +0300

    Add GenerateTokenModel

---
+++ Server/src/Api/Models/GenerateTokenModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace API.Models;
+
+public record GenerateTokenModel(
+    Guid UserId, 
+    List<Claim> Claims, 
+    DateTime ExpireAt,
+    string SecretKey,
+    string Audience,
+    string Issuer
+);
