959c93c4a11aa47d5bb19ffd8562b7daa70ad1ab
velvet_orange <ig348838@gmail.com>
2024-09-19 01:40:09 +0300

    Add ShouldNotSignInUser test

--- Server/src/API.UnitTests/Controllers/AuthControllerUnitTests.cs
+++ Server/src/API.UnitTests/Controllers/AuthControllerUnitTests.cs
@@ -3,6 +3,7 @@ using API.Controllers.Dtos;
 using API.Core.Models;
 using API.Core.Services;
 using FluentResults;
+using Microsoft.AspNetCore.Mvc;
 using NSubstitute;
 
 namespace API.UnitTests.Controllers;
@@ -29,12 +30,42 @@ public class AuthControllerUnitTests
         var requestDto = new SigninRequestDto(login, password);
 
         // Act
-        var result = await controller.SigninAsync(requestDto, ct);
+        var result = await controller.SingInAsync(requestDto, ct);
 
         // Assert
         await userService.Received(1)
             .SingInAsync(Arg.Is<SingInModel>(x => x.Login == login && x.Password == password), ct);
         var expected = new SingInResponseDto(accessToken, refreshToken);
         Assert.Equivalent(expected, result.Value);
+    }
+    [Fact]
+    public async Task ShouldNotSignInUser()
+    {
+        // Arrange
+        const string login = "login";
+        const string password = "password";
+        const string accessToken = "accessToken";
+        const string refreshToken = "refreshToken";
+        var ct = CancellationToken.None;
+        var error = "Test failed";
+        var errors = new List<string>{error};
+        var signInResult = new Result<TokenPairModel>()
+            .WithErrors(errors);
+        var userService = Substitute
+            .For<IUserService>();
+        userService.SingInAsync(Arg.Any<SingInModel>(), ct)
+            .Returns(signInResult);
+        var controller  = new AuthController(userService);
+        var requestDto = new SigninRequestDto(login, password);
+
+        // Act
+        var result = await controller.SingInAsync(requestDto, ct);
+        
+        // Assert
+        await userService.Received(1)
+            .SingInAsync(Arg.Is<SingInModel>(x => x.Login == login && x.Password == password), ct);
+        var conflictResult = Assert.IsType<ConflictObjectResult>(result.Result);
+        var businessError = Assert.IsType<BusinessErrorDto>(conflictResult.Value);
+        Assert.Equivalent(errors, businessError.Messages);
     }
 }
