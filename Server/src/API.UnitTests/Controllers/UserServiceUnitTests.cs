826ef5a2adabbfdea4a46ee62b444c18954a2796
velvet_orange <ig348838@gmail.com>
2024-09-20 01:15:19 +0300

    Add constructor, introduce ShouldDelete test

--- Server/src/API.UnitTests/Controllers/UserServiceUnitTests.cs
+++ Server/src/API.UnitTests/Controllers/UserServiceUnitTests.cs
@@ -1,6 +1,36 @@
-﻿namespace API.UnitTests.Controllers;
+﻿using API.Core.Services;
+using API.Implementation.Services;
+using API.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using NSubstitute;
+
+namespace API.UnitTests.Controllers;
 
 public class UserServiceUnitTests
 {
-    
+    private readonly ApplicationDbContext _dbContext;
+    private readonly ITokenService _tokenService;
+    private readonly IEmailService _emailService;
+    private readonly IHttpContextAccessor _contextAccessor;
+    private readonly UserService _userService;
+
+    public UserServiceUnitTests()
+    {
+        _dbContext = Substitute.For<ApplicationDbContext>();
+        _tokenService = Substitute.For<ITokenService>();
+        _emailService = Substitute.For<IEmailService>();
+        _contextAccessor = Substitute.For<IHttpContextAccessor>();
+        _userService = new UserService(_dbContext, _tokenService, _emailService, _contextAccessor);
+    }
+
+    [Fact]
+    public async Task ShouldDelete()
+    {
+        //Arrange
+        
+        //Act
+        
+        //Assert
+        
+    }
 }
