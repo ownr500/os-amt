1d7fed621d4ae4e492e2085b3bf4a09a2d8c557b
velvet_orange <ig348838@gmail.com>
2024-09-20 18:25:55 +0300

    Add InMemory package, add DBHelper

---
+++ Server/src/API.UnitTests/Helpers/DbHelper.cs
@@ -0,0 +1,23 @@
+﻿using API.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.UnitTests.Helpers;
+
+public class DbHelper
+{
+
+    public static ApplicationDbContext CreateDbContext()
+    {
+        var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        builder.UseInMemoryDatabase(databaseName: "ApplicationDbContextInMemory");
+
+        var dbContextOptions = builder.Options;
+        var dbContext = new ApplicationDbContext(dbContextOptions);
+        
+        dbContext.Database.EnsureDeleted();
+        dbContext.Database.EnsureCreated();
+        dbContext.SaveChanges();
+        
+        return dbContext;
+    }
+}
