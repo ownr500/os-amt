44fda59a4fd25261c8ff5394657fb494fb2f7320
velvet_orange <ig348838@gmail.com>
2024-09-25 02:17:31 +0300

    Add ShouldNotRemoveRoleAsync

--- Server/src/API.UnitTests/Controllers/UserServiceUnitTests.cs
+++ Server/src/API.UnitTests/Controllers/UserServiceUnitTests.cs
@@ -581,5 +581,35 @@ public class UserServiceUnitTests
 
         Assert.False(dbContext.UserRoles.Any(x => x.UserId == user.Id && x.RoleId == RoleConstants.UserRoleId));
         Assert.Equivalent(expected, actual);
+    }    
+    
+    
+    [Fact]
+    public async Task ShouldNotRemoveRoleAsync()
+    {
+        //Arrange
+        var expected = Result.Fail(MessageConstants.UserHasNoRole);
+        
+        var user = new UserEntity
+        {
+            Id = Guid.NewGuid(),
+            UserRoles = new List<UserRoleEntity>()
+        };
+
+        var dbContext = DbHelper.CreateDbContext();
+        dbContext.Users.Add(user);
+        await dbContext.SaveChangesAsync(_ct);
+        var userService = new UserService(dbContext, _tokenService, _emailService, _contextAccessor);
+        
+        //Act
+        var actual = await userService.RemoveRoleAsync(user.Id, Role.User, _ct);
+
+        //Assert
+        await _tokenService.Received(0)
+            .RevokeTokensAsync(user.Id, _ct);
+
+        Assert.False(dbContext.UserRoles.Any(x => x.UserId == user.Id && x.RoleId == RoleConstants.UserRoleId));
+        Assert.Equivalent(expected.IsFailed, actual.IsFailed);
+        Assert.Equivalent(expected.Errors, actual.Errors);
     }
 }
