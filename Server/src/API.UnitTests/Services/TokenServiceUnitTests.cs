d55d193641be94ce7ccd82dc57db2da223c97c90
velvet_orange <ig348838@gmail.com>
2024-09-26 03:30:58 +0300

    Add constructor

--- Server/src/API.UnitTests/Services/TokenServiceUnitTests.cs
+++ Server/src/API.UnitTests/Services/TokenServiceUnitTests.cs
@@ -1,6 +1,17 @@
-﻿namespace API.UnitTests.Controllers;
+﻿using System.IdentityModel.Tokens.Jwt;
+using API.Core.Options;
+using Microsoft.Extensions.Options;
+using NSubstitute;
+
+namespace API.UnitTests.Controllers;
 
 public class TokenServiceUnitTests
 {
-    
+    private readonly JwtSecurityTokenHandler _tokenHandler;
+    private readonly IOptions<TokenOptions> _options;
+    public TokenServiceUnitTests()
+    {
+        _tokenHandler = Substitute.For<JwtSecurityTokenHandler>();
+        _options = Substitute.For<IOptions<TokenOptions>>();
+    }
 }
