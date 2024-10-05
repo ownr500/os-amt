f0ab01b3f5d95751d19e683a3d3bb1a124a04288
velvet_orange <ig348838@gmail.com>
2024-10-05 20:11:14 +0300

    Implement TestDbContext

---
+++ Server/src/API.UnitTests/Helpers/TestDbContext.cs
@@ -0,0 +1,32 @@
+﻿using API.Core.Entities;
+using API.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.UnitTests.Helpers;
+
+public class TestDbContext : ApplicationDbContext
+{
+    public TestDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<TokenEntity>()
+            .Property(x => x.RefreshTokenExpireAt)
+            .HasConversion(u => u.DateTime, 
+                u => new DateTimeOffset(u));
+
+        modelBuilder.Entity<RevokedTokenEntity>()
+            .Property(x => x.TokenExpireAt)
+            .HasConversion(u => u.DateTime,
+                u => new DateTimeOffset(u));
+
+        modelBuilder.Entity<RecoveryTokenEntity>()
+            .Property(x => x.ExpireAt)
+            .HasConversion(u => u.DateTime,
+                u => new DateTimeOffset(u));
+    }
+}
