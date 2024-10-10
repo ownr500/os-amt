8e9dcd21c5f6952870982ce7506051de665bb165
velvet_orange <ig348838@gmail.com>
2024-10-10 18:30:37 +0300

    Update HttpContextService

--- Server/src/Api/Implementation/Services/HttpContextService.cs
+++ Server/src/Api/Implementation/Services/HttpContextService.cs
@@ -1,20 +1,32 @@
 ﻿using System.Security.Claims;
 using API.Core.Services;
+using Microsoft.Extensions.Primitives;
+using Microsoft.Net.Http.Headers;
 
 namespace API.Implementation.Services;
 
 public class HttpContextService : IHttpContextService
 {
     private readonly IHttpContextAccessor _contextAccessor;
+    private readonly HttpContext _context;
 
     public HttpContextService(IHttpContextAccessor contextAccessor)
     {
         _contextAccessor = contextAccessor;
+        _context = _contextAccessor.HttpContext is null
+            ? throw new ArgumentNullException(nameof(_contextAccessor.HttpContext))
+            : _contextAccessor.HttpContext;
+
     }
     public string GetToken()
     {
-        
-        throw new NotImplementedException();
+        if (_context.Request.Headers
+            .TryGetValue(HeaderNames.Authorization, out var header))
+        {
+            return ExtractTokenFromHeader(header);
+        }
+
+        throw new ArgumentNullException(nameof(GetToken));
     }
 
     public Guid GetUserIdFromContext()
@@ -29,5 +41,12 @@ public class HttpContextService : IHttpContextService
         }
 
         return userId;
+    }
+
+    private static string ExtractTokenFromHeader(StringValues header)
+    {
+        var headerArray = header.ToString().Split(' ');
+        if (headerArray.Length != 2) throw new ArgumentNullException(nameof(header));
+        return headerArray[1];
     }
 }
