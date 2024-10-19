0b6ef365903091faaa3c2cea73b5985313d155f7
ownr500 <ismediaspb@gmail.com>
2024-10-20 01:08:04 +0300

    Add EF, db context registration

--- Server/src/Company/Extensions/ApplicationBuilderExtensions.cs
+++ Server/src/Company/Extensions/ApplicationBuilderExtensions.cs
@@ -1,6 +1,13 @@
+using Company.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 namespace Company.Extensions;
 
 internal static class ApplicationBuilderExtensions
 {
-    
+    public static void RegisterDbContext(this WebApplicationBuilder builder)
+    {
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+        builder.Services.AddDbContext<ApplicationDbContext>(x => x.UseSqlServer(connectionString));
+    }
 }
---
+++ Server/src/Company/Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Company.Infrastructure;
+
+public class ApplicationDbContext : DbContext
+{
+    
+}
